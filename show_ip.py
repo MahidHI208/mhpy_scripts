8dc4cfea0966a7782ea0ce67f6870180476d6111
MahidHI208 <heydarimahdi248@gmail.com>
2026-06-24 16:19:25 +0330

    Create show_ip.py

---
+++ show_ip.py
@@ -0,0 +1,13 @@
+import socket
+import requests
+
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
+
+public_ip = requests.get("https://api.ipify.org").text
+
+print(f"🏠 Local IP: {local_ip}")
+print(f"🌍 Public IP: {public_ip}")
+
+
+//Commit new file
